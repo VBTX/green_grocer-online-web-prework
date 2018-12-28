e0e3a9250ae9a85d17d41307ba1f00bf5bd675c9
Victoria B <viktorija.bourassa@gmail.com>
2018-12-28 16:29:27 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -48,7 +48,7 @@ def consolidate_cart(cart)
 def checkout(cart, coupons)
   cart = consolidate_cart(items)
   cart1 = apply_coupons(cart, coupons)
-
+  cart2 = apply_clearance(cart1)
   cart.each do |key, value|
     total = 0
     total += value[:price]
